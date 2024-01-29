40380f62cdd2c6a98b31f7b8e6fbb23bce45883b
harshkumar1302 <bs824536@gmail.com>
2024-01-30 00:26:32 +0530

    date and time

---
+++ basics01/08_datesInJs.js
@@ -0,0 +1,41 @@
+// Dates
+
+// let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+
+// console.log(typeof myDate);
+
+// let createDate = new Date(2023, 0, 21); // yyyy-mm-dd months starts from 0 in an array
+// let createDate = new Date("2023-1-21"); // yyyy-mm-dd
+// let createDate = new Date("1-21-2023"); // mm-dd-yyyy
+// let createDate = new Date(2023, 0, 3);
+
+
+// console.log(createDate);
+// console.log(createDate.toLocaleString());
+
+
+// // time 
+
+// // let myTimeStamp = Date.now();
+// let myTimeStamp = new Date();
+
+// console.log(myTimeStamp.getTime());
+// console.log(createDate.getTime());
+
+
+// // convert to seconds
+
+// console.log(`${Math.floor(createDate.getTime() / 1000)} sec`);
+
+
+
+let newDate1 = new Date();
+console.log(newDate1.getDate());
+console.log(newDate1.getMonth()); // month starts with 0
