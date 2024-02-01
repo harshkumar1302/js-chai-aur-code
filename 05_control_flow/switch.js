448b2ac135f13bf8c70419b6285d2300946ffd09
harshkumar1302 <bs824536@gmail.com>
2024-02-01 10:01:18 +0530

    minor understanding switch

--- 05_control_flow/switch.js
+++ 05_control_flow/switch.js
@@ -29,22 +29,47 @@
 //         break;
 // }
 
-const month = "feb";
+// const month = "feb";
 
-switch (month) {
-    case "jan":
-        console.log("Jan");
-        break;
-    case "feb":
-        console.log("Feb");
-        break;
-    case "march":
-        console.log("March");
-        break;
-    case "april":
-        console.log("April")
-        break;
-    default:
-        console.log("Enter a month number between 1-4");
-        break;
-}
+// switch (month) {
+//     case "jan":
+//         console.log("Jan");
+//         break;
+//     case "feb":
+//         console.log("Feb");
+//         break;
+//     case "march":
+//         console.log("March");
+//         break;
+//     case "april":
+//         console.log("April")
+//         break;
+//     default:
+//         console.log("Enter a month number between 1-4");
+//         break;
+// }
+
+
+
+// const foodRating = 1;
+
+// switch (foodRating) {
+//     case 1:
+//         console.log(`May I know the reason for your ${foodRating} star rating.`);
+//         break;
+//     case 2:
+//         console.log(`May I know the reason for your ${foodRating} star rating.`);
+//         break;
+//     case 3:
+//         console.log(`May I know the reason for your ${foodRating} star rating.`);
+//         break;
+//     case 4:
+//         console.log(`May I know the reason for your ${foodRating} star rating.`);
+//         break;
+//     case 5:
+//         console.log(`May I know the reason for your ${foodRating} star rating.`);
+//         break;
+//     default:
+//         console.log("Please rate with a vaild number between 1-5")
+//         break;
+// }
