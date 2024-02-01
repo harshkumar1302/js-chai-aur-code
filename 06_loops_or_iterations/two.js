5b4e576b1cbca6b7ce87b8b29b6454a44f5330b5
harshkumar1302 <bs824536@gmail.com>
2024-02-01 15:02:09 +0530

    while, do while loop

---
+++ 06_loops_or_iterations/two.js
@@ -0,0 +1,38 @@
+// while loop
+// this loop says = okay so we have the value i = 0 now we check that if the condition is true then only enter the loop
+// or just do not enter it
+
+// let i = 0;
+// while (i <= 10) {
+//     console.log(`The value of i is ${i}`);
+//     i += 2;
+// }
+
+let myArray = ["Superman", "flash", "Aquaman"];
+let arr = 0;
+while (arr < myArray.length) {
+    console.log(`the hero at ${arr + 1} is ${myArray[arr]}`);
+    arr++;
+}
+
+
+// do while = less used loop
+
+// but here in do while loop it says = okay so we have score = 0, now what we have to do ooo we have to print and do the sum
+// so what was the condition i was meant to check ooo score <=10 its true so we will loop
+// and when we get score = 11 after the do part executes then we check the while loop and it says false so we stop over there
+
+// let score = 1;
+
+// do {
+//     console.log(`Score = ${score}`)
+//     score++;
+// } while (score <= 10);
+
+let score = 12;
+
+do {
+    console.log(`Score = ${score}`)
+    score++;         // from here we can 12 but
+} while (score <= 10);  // when we check the condition we get it false so we do not let it loop
+
