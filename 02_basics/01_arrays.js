c3398e22ee8e0c7bfd38129daba94fe16fa035b0
harshkumar1302 <bs824536@gmail.com>
2024-02-01 09:43:57 +0530

    revision of array (.split(""), Array.from(string), .join(""), .reverse())

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,14 +1,14 @@
-// Array
-// it is written inside [] and it holds some elements (it can be number, stings, boolean...)
-// it can be resizable
-// indexing starts from 0
-// shallow copy it created  = a copy where properties shares the same references like heap memory
-// (deep copy = a copy where properties do not share the same references like stack memory) ----- not related to array ------ 
+// // Array
+// // it is written inside [] and it holds some elements (it can be number, stings, boolean...)
+// // it can be resizable
+// // indexing starts from 0
+// // shallow copy it created  = a copy where properties shares the same references like heap memory
+// // (deep copy = a copy where properties do not share the same references like stack memory) ----- not related to array ------
 
 
-const myArr = [0, 2, 5, 4, 8, 9];
-const myHeros = ["saktiman", "chacha chaudhry", "naagraj"];
-const myArr2 = new Array(1, 2, 3, 4, 5, 6);
+// const myArr = [0, 2, 5, 4, 8, 9];
+// const myHeros = ["saktiman", "chacha chaudhry", "naagraj"];
+// const myArr2 = new Array(1, 2, 3, 4, 5, 6);
 
 // console.log(myArr);
 // console.log(myArr2);
@@ -17,7 +17,7 @@ const myArr2 = new Array(1, 2, 3, 4, 5, 6);
 // console.log(myArr[1]);
 
 
-// Array methods
+// // Array methods
 
 // // push pop
 // myArr.push(99);  // this simply pushes or adds a new element at the end of the given array
@@ -38,24 +38,24 @@ const myArr2 = new Array(1, 2, 3, 4, 5, 6);
 // console.log(myArr.indexOf(100)); // -1 means there is no value as 100 in the array
 
 
-// // join
+// // // join
 
 // console.log(myArr);
 // const newArr = myArr.join();
 // console.log(newArr); // here this value of the array has been converted into string by using join() method
 
 
-// slice and splice
+// // slice and splice
 
 // console.log("A", myArr); // origonal array
-// const mArr1 = myArr.slice(1, 3);  // array using slice
-// console.log(mArr1);
+// const mArr3 = myArr.slice(1, 3);  // array using slice =, <
+// console.log(mArr3);
 
 
 // console.log("B", myArr);
-// const mArr2 = myArr.splice(1, 3); // array using splice
+// const mArr4 = myArr.splice(1, 3); // array using splice =, <=
 // console.log("C", myArr); // using splice my original array changes
-// console.log(mArr2);
+// console.log(mArr4);
 
 
 // // splice manupulates the original array by cutting out the the portion given in the argument of the splice method
@@ -66,8 +66,8 @@ const myArr2 = new Array(1, 2, 3, 4, 5, 6);
 // const marvelHeros = ["Ironman", "spiderman", "thor", "hulk"];
 // const dcHeros = ["superman", "batman", "flash", "aquaman"];
 
-// // marvelHeros.push(dcHeros); // this simply put the whole array inside the first array [elements,[elements]]
-// // console.log(marvelHeros); // push works on the existing array
+// marvelHeros.push(dcHeros); // this simply put the whole array inside the first array [elements,[elements]]
+// console.log(marvelHeros); // push works on the existing array
 
 
 // const allHeros = marvelHeros.concat(dcHeros);  // concat creates an new array
@@ -79,21 +79,52 @@ const myArr2 = new Array(1, 2, 3, 4, 5, 6);
 
 
 // const multipleArrays = [1, 2, 3, 4, [5, 6, 7], [8, 9, [0, 1, 5, 9]]];
-// console.log(multipleArrays);
+// // console.log(multipleArrays);
 
 // // const simplifiedArray = multipleArrays.flat(1);
 // const simplifiedArray = multipleArrays.flat(Infinity);  // .flat() spread or simply return an new array with all sub array elements concatenated into it up to specified depth
 // console.log(simplifiedArray);
 
 
-console.log(Array.isArray("Harsh"));  // this checks whether the given data is an array or not
-console.log(Array.from("Harsh")); // this converts the given data into an array
-console.log(Array.from({ name: "Harsh" }));
-// this returns an empty array because it cannot convert it directly (because we have to tell it that from what we have to make an array key or value of the object)
+// console.log(Array.isArray("Harsh"));  // this checks whether the given data is an array or not
+// console.log(Array.from("Harsh")); // this converts the given data into an array
+// console.log(Array.from({ name: "Harsh" }));
+// // this returns an empty array because it cannot convert it directly (because we have to tell it that from what we have to make an array key or value of the object)
 
 
-let score1 = 100;
-let score2 = 500;
-let score3 = 200;
-console.log(Array.of(score1, score2, score3)); // .of() returns a new array from a set of elements (it can be anything number, string, boolean....)
+// let score1 = 100;
+// let score2 = 500;
+// let score3 = 200;
+// console.log(Array.of(score1, score2, score3)); // .of() returns a new array from a set of elements (it can be anything number, string, boolean....)
 
+//----------------------- method calling cannot be done on an simple string ---------------------------------------
+
+// convert a simple string to an array
+
+// const newString = "harsh kumar suman";
+// console.log(newString);
+
+// const newArrayFromString = Array.from(newString);
+// console.log(newArrayFromString);
+
+// // const newArrayUsingSplit = newString.split();
+// const newArrayUsingSplit = newString.split("");
+// console.log(newArrayUsingSplit)
+
+// // const newStr1 = "harsh";
+// // // console.log(Array.from(newStr1));
+// // // console.log(newStr1.split(""));
+
+// // // const needToReverseArr = Array.from(newStr1.split(""));
+// // const needToReverseArr = Array.from(newStr1);
+// // const reverseArr = needToReverseArr.reverse();
+// // console.log(reverseArr.join(""))
+
+// const someChar = ["k", "i", "t", "e"];
+// console.log(someChar);
+// console.log(someChar.join("").toUpperCase());
+
+
+// const nums = [1, 5, 78, 9];
+// console.log(nums.reverse());
+// console.log(nums.join(""))
