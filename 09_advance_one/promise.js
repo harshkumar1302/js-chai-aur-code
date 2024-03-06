3aec98b1e40b0ab655c8514d3a30f9155e214e61
harshkumar1302 <bs824536@gmail.com>
2024-03-07 00:51:42 +0530

    Promise + aysnc (try catch)

---
+++ 09_advance_one/promise.js
@@ -0,0 +1,82 @@
+//CREATING AND CONSUMING PROMISE
+
+const promiseOne = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        console.log("Aysnc task compeleted");
+        resolve();
+    }, 1000)
+})
+
+promiseOne.then(function () {
+    console.log("Promise consumed")
+});
+
+
+new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        console.log("Aysnc 2 compeleted")
+        resolve();
+    }, 1000)
+}).then(function () {
+    console.log("Promise 2 also consumed")
+})
+
+
+
+const promiseThree = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve({ username: "chai", email: "chai@example.com" });
+    }, 1000);
+});
+
+
+promiseThree.then((user) => { console.log(user.username) });
+
+
+
+
+const promiseFour = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        let error = false;
+        if (!error) {
+            resolve({ userName: "Harsh", password: "123qwe" })
+        } else {
+            reject("ERROR! JS went wrong")
+        }
+    }, 1000)
+})
+
+
+promiseFour.then((user) => {
+    return user.userName;
+}).then((username) => {
+    console.log(username);
+}).catch((err) => {
+    console.log(err)
+});
+
+
+
+const promiseFive = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        let error = true;
+        if (!error) {
+            resolve({ userName: "kumar", password: "123" })
+        } else {
+            reject("ERROR! JS went wrong")
+        }
+    }, 1000)
+});
+
+
+
+async function consumePromiseFive() {
+    try {
+        const response = await promiseFive;
+        console.log(response);
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+consumePromiseFive();
